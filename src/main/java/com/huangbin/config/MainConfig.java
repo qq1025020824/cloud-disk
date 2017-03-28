344b37659eff82684bc04345c1fe7d1945bdb977
Huanngbin <18819451379@163.com>
2017-03-28 12:50:10 +0800

    first commit,add springmvc ,maven configuration,pesistent component

---
+++ src/main/java/com/huangbin/config/MainConfig.java
@@ -0,0 +1,12 @@
+package com.huangbin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Created by Huangbin on 2017/3/23.
+ */
+@Configuration
+@EnableTransactionManagement
+public class MainConfig {
+}
