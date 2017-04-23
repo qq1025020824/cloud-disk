49ce3d986e1a1a204f55eaa6595d284b718c982e
qq1025020824 <1025020824@qq.com>
2017-04-23 17:13:12 +0800

    2017/4/23/2

--- src/main/java/com/abo/controller/FileController.java
+++ src/main/java/com/abo/controller/FileController.java
@@ -8,12 +8,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.abo.model.MyFile;
 import com.abo.service.FileService;
 
 @Controller
@@ -43,7 +45,35 @@ public class FileController extends BaseController{
 		return "redirect:/file/upload";
 	}
 	
-
+	/**
+	 * @param folder 文件夹id,用户根目录为home
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/view")
+	public String showFolder(@RequestParam String folder,Model model){
+		//取用户id
+		Long userid=this.getUserID();
+		if(userid==null){
+			this.sendMessage("尚未登录，请先登录。");
+			return "redirect:/login";
+		}
+		//转换fileid
+		Long fileid;
+		if(folder.equals("home")){
+			MyFile file=fileService.getUserRoot(userid);
+			fileid = file.getId();
+		}else{
+			fileid=new Long(folder);
+		}
+		//判断用户是否有访问权限
+		if(!fileService.checkUserFile(userid, fileid)){
+			//无权访问，跳转
+			return "redirect:/file/view?folder=home";
+		}
+		//获取路径列
+		return "";
+	}
 
 	//test
 	@RequestMapping(value = "/test1")
--- src/main/java/com/abo/service/FileService.java
+++ src/main/java/com/abo/service/FileService.java
@@ -1,11 +1,15 @@
 package com.abo.service;
 
+import java.util.Stack;
+
 import javax.annotation.Resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.abo.dao.FileDao;
+import com.abo.model.MyFile;
+import com.abo.vo.MyFileVO;
 
 
 public class FileService {
@@ -13,4 +17,34 @@ public class FileService {
 	private FileDao fileDao;
 	private static final Logger Log = LoggerFactory.getLogger(FileService.class);
 
+	//未写
+	
+	/**
+	 * 判断用户是否能访问某文件
+	 * 包含文件不存在情形
+	 * @param userid
+	 * @param fileid
+	 * @return
+	 */
+	public boolean checkUserFile(Long userid,Long fileid){
+		return false;
+	}
+	
+	/**
+	 * 取出用户根目录
+	 * @param userid
+	 * @return
+	 */
+	public MyFile getUserRoot(Long userid){
+		return null;
+	}
+	
+	/**
+	 * 获取路径栈用于展示
+	 * @param folderid
+	 * @return
+	 */
+	public Stack<MyFileVO> getPathByFolderID(Long folderid){
+		return null;
+	}
 }
