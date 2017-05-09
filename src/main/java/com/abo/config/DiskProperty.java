ca41d7dd8c83567d21882c4326d2cea75c4a6421
qq1025020824 <1025020824@qq.com>
2017-05-09 17:34:02 +0800

    2017/5/9 修改DiskProperty

--- src/main/java/com/abo/config/DiskProperty.java
+++ src/main/java/com/abo/config/DiskProperty.java
@@ -2,11 +2,14 @@ package com.abo.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DiskProperty {
+@Lazy(false)
+public class DiskProperty implements InitializingBean{
 	private static final Logger Log = LoggerFactory.getLogger(DiskProperty.class);
 	
 	@Value("${DISK_PATH}")
@@ -16,7 +19,7 @@ public class DiskProperty {
 	@Value("${DEFAULT_TOTAL_SIZE_UNIT}")
 	private String unit;
 	
-	private static int totalsize=0;
+	private int totalsize;
 	
 	public String getDiskpath() {
 		Log.debug("[ABO][getDiskpath()]diskpath:{}", diskpath);
@@ -24,24 +27,29 @@ public class DiskProperty {
 	}
 
 	public int getTotalsize() {
-		if (totalsize == 0) {
-			Log.debug("[ABO][init totalsize]");
-			totalsize = Integer.parseInt(size);
-			switch (unit) {
-			case "TB":
-				totalsize *= 1024;
-			case "GB":
-				totalsize *= 1024;
-			case "MB":
-				totalsize *= 1024;
-			case "KB":
-				totalsize *= 1024;
-			case "B":
-			default:
-				break;
-			}
-			Log.debug("[ABO]totalsize:{}", totalsize);
-		}
+		Log.debug("[ABO][getTotalsize()]{}", totalsize);
 		return totalsize;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		Log.debug("[ABO][afterPropertiesSet()]diskpath:{}", diskpath);
+		Log.debug("[ABO][init totalsize]");
+		totalsize = Integer.parseInt(size);
+		switch (unit) {
+		case "TB":
+			totalsize *= 1024;
+		case "GB":
+			totalsize *= 1024;
+		case "MB":
+			totalsize *= 1024;
+		case "KB":
+			totalsize *= 1024;
+		case "B":
+		default:
+			break;
+		}
+		Log.debug("[ABO]totalsize:{}", totalsize);
 	}
 }
